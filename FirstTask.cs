bc8e378b8b5c6f62bd8e9d6f0ba8110ed40f56d8
kiruko <dragonkali1@mail.ru>
2023-06-28 16:21:09 +0300

    Added input conditions

--- FirstTask.cs
+++ FirstTask.cs
@@ -2,10 +2,20 @@ namespace Library
 {
     public class ZadanieOne
     {
-        public void One()
+        public static void One()
         {
+            int n;
             Console.Write("Введите число N: ");
-            int n = int.Parse(Console.ReadLine());
+            if (!(int.TryParse(Console.ReadLine(), out n)))
+            {
+                Console.WriteLine("Некорректный ввод."); //If N is not a number, exit the function.
+                return;
+            }
+            else if (n < 1)
+            {
+                Console.WriteLine("Введённое число меньше единицы."); //If N < 1, exit the function.
+                return;
+            }
             string result = string.Join(", ", Enumerable.Range(1, n));
             Console.WriteLine(result);
         }
@@ -15,7 +25,17 @@ namespace Library
         public static void Two()
         {
             Console.Write("Введите нечетное число N: ");
-            int n = int.Parse(Console.ReadLine());
+            int n;
+            if (!(int.TryParse(Console.ReadLine(), out n)))
+            {
+                Console.WriteLine("Некорректный ввод."); //If N is not a number, exit the function.
+                return;
+            }
+            if (n < 3)
+            {
+                Console.WriteLine("Введённое число меньше трёх."); //If N < 3, exit the function.
+                return;
+            }
             if (n % 2 == 0) // If N is an even number, exit the function.
             {
                 Console.WriteLine("Вы ввели четное N.");
